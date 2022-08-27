1db2b3a3bf02fc2767dd1f0f7975728805890ca3
久保菜友加 <srkm.149@gmail.com>
2022-08-27 22:13:59 +0900

    論理演算子

--- practice.rb
+++ practice.rb
@@ -14,10 +14,17 @@ puts "Hello,#{name}!"
 puts 'ruby' != 'perl'
 # .bytes （配列で表示）
 puts 'abc'.bytes
-# if条件分岐 Rubyの場合 falseとnil以外全て真
+=begin
+if条件分岐 Rubyの場合 falseとnil以外全て真
 data = find_data
 if data
   'データがあります'
 else
   'データがありません'
 end
+=end
+# 論理演算子　条件&&条件(かつ) 条件||条件(または)
+t1 = true
+t2 = false
+puts t1&&t2
+puts t1||t2
